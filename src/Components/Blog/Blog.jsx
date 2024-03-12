9b88a644a5e5c2c7c646a2576ca2776b9038dec2
Naiemjoy1 <naiemjoy1@gmail.com>
2024-03-13 01:56:18 +0600

    image

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,32 @@
 import PropTypes from 'prop-types';
+// import { FaBeer } from 'react-icons/fa';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+
+    const {title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
+
     return (
-        <div>
-            
+        <div className=' mb-20'>
+            <img className=' w-full mb-8' src={cover} alt={`cover picture of ${title}`} />
+            <div className="flex justify-between mb-4">
+                <div className='flex'>
+                    <img className='w-14 h-14 rounded-full ' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl '>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button>Bookmark</button>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href=''>#{hash}</a></span> )
+                }
+            </p>
         </div>
     );
 };
